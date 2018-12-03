8cde5cf58d734c583b6dd80611fc1b035ddbdc3e
gavinzhou <gavin.zhou@gmail.com>
2018-12-03 20:14:36 +0900

    automate install gcloud sdk

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,3 +1,8 @@
-yes | curl https://sdk.cloud.google.com | bash
+FILE="google-cloud-sdk-101.0.0-linux-x86_64.tar.gz"
+LINK="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/"
+cd ${HOME}
+curl -L "$link""$file" | tar xz
+CLOUDSDK_CORE_DISABLE_PROMPTS=1 ${HOME}/google-cloud-sdk/install.sh
+
 y | curl -L https://git.io/n-install | bash
 \curl -sSL https://get.rvm.io | bash
