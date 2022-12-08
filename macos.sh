d50651e1449b884884673c4d3effea3f5afae044
gavinzhou <gavin.zhou@gmail.com>
2022-12-08 23:35:48 +0900

    fixed Trackpad setting

--- macos.sh
+++ macos.sh
@@ -3,3 +3,5 @@
 # Map Caps Lock key to Left Control key
 # https://developer.apple.com/library/content/technotes/tn2450/_index.html
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0}]}'
+
+defaults -currentHost write -g com.apple.mouse.tapBehavior 1
